01b5633b1a7b497d5be7ca47f6bbafff1549ce8b
clay-richards <65131538+clay-richards@users.noreply.github.com>
2025-01-04 16:32:52 +0900

    Add files via upload

---
+++ PythonFundamentals/ConditionandBlockBasic.py
@@ -0,0 +1,9 @@
+
+myName = 'Clay'     #condition#
+password = 'pookies' #condition#
+if myName == 'Clay':      #blocks from here down#
+    print('Hello Clay')
+    if password == 'pookies' :
+        print('Access granted.')
+    else:
+        print('Wrong password.')
---
+++ PythonFundamentals/elifStatement.py
@@ -0,0 +1,10 @@
+name = 'clay'
+age = 112
+if name == 'clay':
+    print('hello clay')
+elif age < 12:
+    print('youngin err')
+elif age > 2000:
+    print('yeah okayyy')
+elif age > 100:
+    print('how was wwI?')
