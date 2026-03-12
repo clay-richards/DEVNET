53e87593b3b70873ae273d101a9b6cf4f92347b2
clay-richards <clay.richards2222@live.com>
2026-03-12 17:17:39 +0900

    Updated devices file

---
+++ devnet_phase1/netmiko/devices.py
@@ -0,0 +1,14 @@
+devices = [
+{
+  "device_type": "cisco_ios",
+  "host": "x.x.x.x",
+  "username": "admin",
+  "password": "cisco",
+},
+{
+  "device_type": "cisco_ios",
+  "host": "x.x.x.x",
+  "username": "admin",
+  "password": "cisco",
+}
+]
