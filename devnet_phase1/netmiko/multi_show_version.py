2201a11b37221871a8cbe439d2eac3db8001e720
clay-richards <clay.richards2222@live.com>
2026-03-12 17:24:55 +0900

    new

--- devnet_phase1/netmiko/multi_show_version.py
+++ devnet_phase1/netmiko/multi_show_version.py
@@ -1,3 +1,15 @@
 from netmiko import ConnectHandler
 from devices import devices
 
+for device in devices:
+  
+  print(f"connecting to {device['host']}")
+  
+  connection = ConnectHandler(**device)
+  
+  output = connection.send_command("sh ver")
+  
+  print(output)
+  
+  connection.disconnect()
+
